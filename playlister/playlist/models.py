bebf2d8e0920f35b6a65eb34e5470e627b710b87
squeaky1273 <pjriddell22@gmail.com>
2019-12-02 14:53:32 -0800

    added to models

--- playlister/playlist/models.py
+++ playlister/playlist/models.py
@@ -1,3 +1,24 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Playlists(models.Model):
+    playlist_name = models.CharField(max_length=200)
+    video_link = models.CharField(max_length=200, blank=True, default='')
+    author = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    
+    def __str__(self):
+        return self.playlist_name
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
+class Videos(models.Model):
+    playlist = models.ForeignKey(Playlists, on_delete=models.CASCADE)
--- playlister/playlister/urls.py
+++ playlister/playlister/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,6 +22,7 @@ urlpatterns = [
     # Accounts
     #path('accounts/', include('django.contrib.auth.urls')),
     #path('accounts/', include('accounts.urls')),
-]
+    
     # Playlist App
-    #path('', include('playlist.urls')),
+    #path('playlist', include('playlist.urls'))
+    ]
