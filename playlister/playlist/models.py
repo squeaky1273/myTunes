4d9aabb62e39579fc3caeb274a43a18efcc2c477
squeaky1273 <pjriddell22@gmail.com>
2019-12-03 11:22:42 -0800

    fixed variable name in models

--- playlister/playlist/models.py
+++ playlister/playlist/models.py
@@ -16,17 +16,15 @@ class Playlist(models.Model):
     def __str__(self):
         return self.playlist_name
 
-    def was_published_recently(self):
-        now = timezone.now()
-        return now - datetime.timedelta(days=1) <= self.pub_date <= now
-    was_published_recently.admin_order_field = 'pub_date'
-    was_published_recently.boolean = True
-    was_published_recently.short_description = 'Published recently?'
+    def get_absolute_url(self):
+        """ Returns a fully-qualified path for a playlist (/my-new-playlist-page). """
+        path_components = {'slug': self.slug}
+        return reverse('playlist-details-page', kwargs=path_components)
 
     def save(self, *args, **kwargs):
         """ Creates a URL safe slug automatically when a new a playlist is created. """
         if not self.pk:
-            self.slug = slugify(self.title, allow_unicode=True)
+            self.slug = slugify(self.playlist_name, allow_unicode=True)
 
         # Call save on the superclass.
         return super(Playlist, self).save(*args, **kwargs)
