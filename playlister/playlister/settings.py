0066901951b130053d08e711611437a13d3d4d1a
squeaky1273 <pjriddell22@gmail.com>
2019-12-05 19:07:17 -0800

    fixed template error

--- playlister/playlister/settings.py
+++ playlister/playlister/settings.py
@@ -58,7 +58,9 @@ ROOT_URLCONF = 'playlister.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates').replace('\\', '/'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
