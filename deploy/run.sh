790358eea8597627bcdc00d357a6121fcaccfbd7
squeaky1273 <pjriddell22@gmail.com>
2020-05-11 16:31:05 -0700

    made fixes to Dcoker

--- deploy/run.sh
+++ deploy/run.sh
@@ -1,2 +1,3 @@
-docker image build --compress -t docker-django-image:latest . && \
-docker container run --publish 8080:8080 --env PROJECT_DB_HOST=docker.for.mac.host.internal --env DEBUG=True --env COMPRESS_ENABLED=True --env COMPRESS_OFFLINE=True --name=docker-django docker-django-image:latest
+python manage.py collectstatic --noinput
+python manage.py migrate
+gunicorn playlister.wsgi --bind=0.0.0.0:80
