250fb15eb10afd10fdf1aa8e4c851db4dbc2a8d1
squeaky1273 <pjriddell22@gmail.com>
2020-05-13 01:33:28 -0700

    [attempt] Dockerfile

--- playlister/settings.py
+++ playlister/settings.py
@@ -93,17 +93,18 @@ WSGI_APPLICATION = 'playlister.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME':  os.getenv("PROJECT_DB_NAME"),
-        'USER':  os.getenv("PROJECT_DB_USER"),
-        'PASSWORD': os.getenv("PROJECT_DB_PASSWORD"),
-        'HOST': os.getenv("PROJECT_DB_HOST"),
-        'PORT': os.getenv("PROJECT_DB_PORT"),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME':  os.getenv("PROJECT_DB_NAME", "postgres"),
+    #     'USER':  os.getenv("PROJECT_DB_USER", "postgres"),
+    #     'PASSWORD': os.getenv("PROJECT_DB_PASSWORD", "postgres"),
+    #     'HOST': os.getenv("PROJECT_DB_HOST", 'db'),
+    #     'PORT': os.getenv("PROJECT_DB_PORT", 5432),
     }
 }
 
